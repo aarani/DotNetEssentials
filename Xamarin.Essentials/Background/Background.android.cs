0cc1134531e812ec9d7909f789451c8769f7b708
Afshin Arani <afshinarani79@gmail.com>
2020-10-15 18:10:59 +0330

    Initial Commit for background tasks

---
+++ Xamarin.Essentials/Background/Background.android.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Android.Content;
+
+namespace Xamarin.Essentials.Background
+{
+    public static partial class Background
+    {
+        static ContextWrapper context;
+
+        public static void Initialize(ContextWrapper context)
+        {
+            Background.context = context;
+        }
+
+        public static void StartBackgroundService()
+        {
+            var intent = new Intent(context, typeof(BackgroundService));
+            context.StartService(intent);
+        }
+    }
+}
